ee93184f5ab825d4026eaa8c087829cf5f5cfebc
abdul565656 <abdullahkashif12b3@gmail.com>
2024-03-09 11:26:38 +0500

    work complete

---
+++ main.js
@@ -0,0 +1,25 @@
+///////HOMEWORK 1/////////
+// let a:number = 5;
+// let b:number = 2;
+// let c:number  =
+//   a++ + a++ + --b + b-- + a + b++ + b;
+//   console.log(c);
+// 5 + 6 + 1 + 1 + 7 + 0 + 1//
+///////HOMEWORK 2/////////
+//  let a:number = 5;
+//  let b:number = 2;
+//  let c:number  =
+//    ++a + a++ + --b + b-- + a + b++ + b;
+//    console.log(c);
+// 6 + 6 + 1 + 1 + 7 + 0 +1
+////////HOMEWORK 3//////////
+var a = 5;
+var b = 2;
+var isequalto = (a == b); //false
+var notequalto = (a != b); //true
+var isgreaterthan = (a > b); //false 
+var islessthan = (a < b); //true
+console.log(isequalto);
+console.log(notequalto);
+console.log(isgreaterthan);
+console.log(islessthan);
---
+++ main.ts
@@ -0,0 +1,27 @@
+///////HOMEWORK 1/////////
+// let a:number = 5;
+// let b:number = 2;
+// let c:number  =
+//   a++ + a++ + --b + b-- + a + b++ + b;
+//   console.log(c);
+// 5 + 6 + 1 + 1 + 7 + 0 + 1//
+
+///////HOMEWORK 2/////////
+//  let a:number = 5;
+//  let b:number = 2;
+//  let c:number  =
+//    ++a + a++ + --b + b-- + a + b++ + b;
+//    console.log(c);
+   // 6 + 6 + 1 + 1 + 7 + 0 +1
+
+////////HOMEWORK 3//////////
+let a:number = 5;
+let b:number = 2;   
+let isequalto = (a == b);//false
+let notequalto = (a != b);//true
+let isgreaterthan = (a > b);//false 
+let islessthan = (a < b);//true
+console.log(isequalto);
+console.log(notequalto);
+console.log(isgreaterthan);
+console.log(islessthan);
